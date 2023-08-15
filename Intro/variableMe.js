6a56c76f4f8f83a5b625d6e957fa5c659a03666a
Malik Muhammad Usman <usmaan.maalik9503@gmail.com>
2023-08-15 12:55:54 +0500

    understanding the any keyword

--- Intro/variableMe.js
+++ Intro/variableMe.js
@@ -1,2 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello World";
+var num = 6;
+//number
+var userId = 1234;
+var userId2 = 100.20;
+var myId = 10003; //you don't need to infer the type, typescript automatically detect it when you first assign it the number
+//boolean
+var isLogin = false;
+greetings.toLowerCase();
 console.log(greetings);
--- Intro/variableMe.ts
+++ Intro/variableMe.ts
@@ -2,8 +2,30 @@ let greetings: string = "Hello World";
 
 let num = 6
 
+//number
+let userId: number = 1234
+let userId2: number = 100.20
+
+let myId = 10003 //you don't need to infer the type, typescript automatically detect it when you first assign it the number
+
+//boolean
+let isLogin: boolean = false
+
 greetings.toLowerCase()
 
 console.log(greetings);
+
+
+//any
+let hero
+// let hero: number
+
+function getHero(){
+  //both return statements are correct
+  //return 200
+  return "hello"
+}
+
+hero = getHero()
 
 export {}
