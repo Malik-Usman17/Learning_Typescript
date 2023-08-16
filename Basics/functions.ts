cf4618a0e6c66e938122371c0c5e14c92f53940a
Malik Muhammad Usman <malik.usman@sybrid.com>
2023-08-16 12:42:55 +0500

    working on functions

--- Basics/functions.ts
+++ Basics/functions.ts
@@ -6,4 +6,51 @@ function addTwo(num: number){
 addTwo(20)
 // addTwo("20")  //it gives error if we specifically define type
 
+//in functions definig data type is necessary
+function getUpper(val: string) {
+  return val.toUpperCase()
+}
+
+getUpper("MALIK")
+
+function sinUpUser(name: string, email: string, isPaid: boolean){}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+sinUpUser("malik", "malik@mail.com", false)
+loginUser("usman", "m.mcom")
+
+function addingNumbers(num: number){
+  return "hello"
+}
+
+addingNumbers(10)
+//there is no in addingNumbers function creation and calling, but i also want to restrict
+//the type on returning just like the passing an argument
+
+function addNum(n: number): number{
+  return n + 1
+}
+
+addNum(100)
+
+const getHello = (s: string): string => {
+  return "hello"
+}
+
+const heros = ["messi", "kroos", "ronaldo"]
+// const heros = [1, 2, 3]
+heros.map((val): string => {
+  // return 200
+  return `Favorite footballer is ${val}`
+})
+
+function consoleErrors(err: string): void{
+  console.log(err)
+}
+
+const handleErrors = (err: string): never => {
+  throw new Error(err)
+}
+
 export {}
