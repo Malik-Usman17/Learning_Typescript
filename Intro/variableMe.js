b4e1fe56c94d3ebe3abdfb512fa2a29a0b26a83c
DELL <usmaan.maalik9503@gmail.com>
2023-11-26 22:46:44 +0100

    understanding variables declaration in typescript

--- Intro/variableMe.js
+++ Intro/variableMe.js
@@ -1,12 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello World";
+var myData = "usman";
 var num = 6;
 //number
 var userId = 1234;
-var userId2 = 100.20;
+var userId2 = 100.20; //it's too obvious here you don't need to assign type number
 var myId = 10003; //you don't need to infer the type, typescript automatically detect it when you first assign it the number
+myId.toFixed(1);
+userId2.toFixed(1);
 //boolean
 var isLogin = false;
 greetings.toLowerCase();
 console.log(greetings);
+//any
+var hero;
+// let hero: number
+function getHero() {
+    //both return statements are correct
+    //return 200
+    return "hello";
+}
+hero = getHero();
+var myVal; //in this scenario you can mention type
+function getMyVal() {
+    return "false";
+}
+myVal = getMyVal();
--- Intro/variableMe.ts
+++ Intro/variableMe.ts
@@ -1,12 +1,19 @@
 let greetings: string = "Hello World";
 
+let myData = "usman"
+
+
+
 let num = 6
 
 //number
 let userId: number = 1234
-let userId2: number = 100.20
+let userId2: number = 100.20  //it's too obvious here you don't need to assign type number
 
 let myId = 10003 //you don't need to infer the type, typescript automatically detect it when you first assign it the number
+myId.toFixed(1)
+
+userId2.toFixed(1)
 
 //boolean
 let isLogin: boolean = false
@@ -14,7 +21,6 @@ let isLogin: boolean = false
 greetings.toLowerCase()
 
 console.log(greetings);
-
 
 //any
 let hero
@@ -27,5 +33,13 @@ function getHero(){
 }
 
 hero = getHero()
+
+let myVal : string;  //in this scenario you can mention type
+
+function getMyVal(){
+  return "false"
+}
+
+myVal = getMyVal()
 
 export {}
